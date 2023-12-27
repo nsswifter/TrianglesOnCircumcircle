455b842340f08fdfe168d7107a2c5d209cc1fa01
Mehdi Karami <karami.mehdi.work@github.com>
2023-12-28 00:19:42 +0330

    Add Global Properties

--- TrianglesOnCircumcircle.playground/Contents.swift
+++ TrianglesOnCircumcircle.playground/Contents.swift
@@ -3,11 +3,18 @@
 import AppKit
 import PlaygroundSupport
 
-let nibFile = NSNib.Name("MyView")
-var topLevelObjects : NSArray?
+// MARK: - Load Nib File of Main Bundle
+
+private let nibFile = NSNib.Name("MyView")
+private var topLevelObjects : NSArray?
 
 Bundle.main.loadNibNamed(nibFile, owner:nil, topLevelObjects: &topLevelObjects)
-let views = (topLevelObjects as! Array<Any>).filter { $0 is NSView }
+private let views = (topLevelObjects as! Array<Any>).filter { $0 is NSView }
+
+// MARK: - Global Properties
+
+private var circumcenter = CGPoint()
+private var coordinatePlaneView = NSView()
 
 // Present the view in Playground
 PlaygroundPage.current.liveView = views[0] as! NSView
