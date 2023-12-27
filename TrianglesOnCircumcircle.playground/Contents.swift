94b04040ad9259fd4742d94ee269c0b7469ca888
Mehdi Karami <karami.mehdi.work@github.com>
2023-12-28 00:22:24 +0330

    Setup Triangles On Coordinate Plane

--- TrianglesOnCircumcircle.playground/Contents.swift
+++ TrianglesOnCircumcircle.playground/Contents.swift
@@ -38,8 +38,35 @@ if let mainView = views.first as? NSView {
                                        y: coordinatePlaneView.bounds.height / 2)
                 
                 // Setup Triangles On Coordinate Plane
+                refreshTrianglesOnCoordinatePlane(numberOfTriangles: 3)
             }
         }
+    }
+}
+
+// MARK: - Methods
+
+/// Refreshes the display of triangles on the coordinate plane.
+///
+/// - Parameters:
+///   - numberOfTriangles: The number of triangles to generate and display.
+func refreshTrianglesOnCoordinatePlane(numberOfTriangles: Int) {
+    // Removes all Triangles from the view.
+    coordinatePlaneView.removePointViews()
+    coordinatePlaneView.removeLines()
+    
+    // Create an instance of CircumcircleTriangleGenerator
+    let circumcircleTriangleGenerator = CircumcircleTriangleGenerator()
+    // Generate points on the circumcircle using the CircumcircleTriangleGenerator
+    let points = circumcircleTriangleGenerator.generateTriangles(circumcenter: circumcenter,
+                                                                 circumradius: 100,
+                                                                 numberOfTriangles: numberOfTriangles)
+    
+    // Iterate through the generated points
+    for point in points {
+        let color = NSColor.random
+        coordinatePlaneView.drawPoint(from: point, withColor: color)
+        coordinatePlaneView.drawLines(from: point, withColor: color)
     }
 }
 
